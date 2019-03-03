880121db8cffeaa9e8f9441f1a4007a4225e8e80
Михаил Кормановский <kormanowsky@gmail.com>
2019-03-04 00:30:04 +0300

    Removed name from adapters

--- adapters.py
+++ adapters.py
@@ -1,33 +1,34 @@
 import inspect
 
+
 class BaseAdapter:
 
     def __init__(self):
         pass
 
     # This must be overridden in child classes
-    def adapter_fn(self, name, value):
-        return "{} = {}".format(str(name), str(value))
+    def adapter_fn(self, value):
+        return str(value)
 
-    def adapt(self, name, value):
-        return self.adapter_fn(name, value)
+    def adapt(self, value):
+        return self.adapter_fn(value)
 
 
 class StrAdapter(BaseAdapter):
 
-    def adapter_fn(self, name, value):
+    def adapter_fn(self, value):
         value = value.replace('"', '\\"')
         value = "\"{}\"".format(value)
-        return "{} = {}".format(str(name), str(value))
+        return value
 
 
 class FunctionAdapter(BaseAdapter):
 
-    def adapter_fn(self, name, value):
+    def adapter_fn(self, value):
         value_lines = [""] + inspect.getsourcelines(value)[0]
         for line_num, line in enumerate(value_lines):
             if line_num == 1:
-                line = line.replace(line.split(" ")[1].split("(")[0], name)
+                line = line.replace(line.split(" ")[1].split("(")[0], "__name_placeholder__")
             line = line.replace("\n", "")
             value_lines[line_num] = line
         return "\n".join(value_lines)
